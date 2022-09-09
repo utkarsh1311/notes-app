010ff79e0cc0f119a60d62cd1790ac9ab3268af6
Utkarsh <itsutkarsh123@gmail.com>
2022-09-09 19:52:35 +0530

    add simple test

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+	env: {
+		commonjs: true,
+		es2021: true,
+		node: true,
+		jest: true,
+	},
+	extends: "eslint:recommended",
+	overrides: [],
+	parserOptions: {
+		ecmaVersion: "latest",
+	},
+	rules: {
+		indent: ["error", "tab"],
+		"linebreak-style": ["error", "unix"],
+		quotes: ["error", "double"],
+		semi: ["error", "always"],
+		"no-trailing-spaces": "error",
+		"object-curly-spacing": ["error", "always"],
+		"arrow-spacing": ["error", { before: true, after: true }],
+		"no-console": 0,
+	},
+};
--- mongo.js
+++ mongo.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
 if (process.argv.length < 3) {
-    console.log(
-        "Please provide the password as an argument: node mongo.js <password>"
-    );
-    process.exit();
+	console.log(
+		"Please provide the password as an argument: node mongo.js <password>"
+	);
+	process.exit();
 }
 
 const password = process.argv[2];
@@ -12,32 +12,32 @@ const password = process.argv[2];
 const url = `mongodb+srv://fullstackopen:${password}@cluster0.jdsphjo.mongodb.net/noteApp?retryWrites=true&w=majority`;
 
 const noteSchema = new mongoose.Schema({
-    content: String,
-    date: Date,
-    important: Boolean,
+	content: String,
+	date: Date,
+	important: Boolean,
 });
 
 const Note = mongoose.model("Note", noteSchema);
 
 mongoose
-    .connect(url)
-    //.then(result => {
-    // console.log('connected');
-    //const note = new Note({
-    //  content: "HTML is easy",
-    // date: new Date(),
-    // important: true,
-    // })
-    //return note.save()
-    //})
-    //.then(() => {
-    //console.log('note saved')
-    //  return mongoose.connection.close();
-    //})
-    //.catch(err => console.log(err));
-    .then(() => {
-        Note.find({}).then(result => {
-            result.forEach(note => console.log(note));
-            mongoose.connection.close();
-        });
-    });
+	.connect(url)
+	//.then(result => {
+	// console.log('connected');
+	//const note = new Note({
+	//  content: "HTML is easy",
+	// date: new Date(),
+	// important: true,
+	// })
+	//return note.save()
+	//})
+	//.then(() => {
+	//console.log('note saved')
+	//  return mongoose.connection.close();
+	//})
+	//.catch(err => console.log(err));
+	.then(() => {
+		Note.find({}).then(result => {
+			result.forEach(note => console.log(note));
+			mongoose.connection.close();
+		});
+	});
