ef08d8eb7a92021fbc022701cb8da9698f5e59b5
Utkarsh <itsutkarsh123@gmail.com>
2022-10-02 23:33:27 +0530

    modify post route of notes to add user info

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,5 +1,6 @@
 const notesRouter = require("express").Router();
 const Note = require("../models/note");
+const User = require("../models/user");
 
 notesRouter.get("/", async (req, res) => {
 	const notes = await Note.find({});
@@ -17,14 +18,19 @@ notesRouter.get("/:id", async (req, res, next) => {
 
 notesRouter.post("/", async (req, res, next) => {
 	const body = req.body;
+	const user = await User.findById(body.userId);
 
 	const note = new Note({
 		content: body.content,
 		important: body.important || false,
 		date: new Date(),
+		user: user._id,
 	});
 
 	const savedNote = await note.save();
+	user.notes = user.notes.concat(savedNote._id);
+	await user.save();
+
 	res.status(201).json(savedNote);
 });
 
