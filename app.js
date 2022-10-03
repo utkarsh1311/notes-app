9ff6b5e2f76259fc744571d635675ef7aea5c6e0
Utkarsh <itsutkarsh123@gmail.com>
2022-10-03 21:22:52 +0530

    add loginRouter to app

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const middleware = require("./utils/middleware");
 const logger = require("./utils/logger");
 const mongoose = require("mongoose");
 const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
 
 logger.info(`connecting to ${config.MONGODB_URI}`);
 
@@ -26,6 +27,7 @@ app.use(express.json());
 app.use(middleware.requestLogger);
 app.use("/api/users/", usersRouter);
 app.use("/api/notes", notesRouter);
+app.use("/api/login", loginRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
