53350a69115b2f79e11205f8933c5e236d8a6610
Utkarsh <itsutkarsh123@gmail.com>
2022-10-04 22:13:16 +0530

    add token auth

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,13 +1,14 @@
 const notesRouter = require("express").Router();
 const Note = require("../models/note");
 const User = require("../models/user");
+const jwt = require("jsonwebtoken");
 
 notesRouter.get("/", async (req, res) => {
 	const notes = await Note.find({}).populate("user", { username: 1, name: 1 });
 	res.json(notes);
 });
 
-notesRouter.get("/:id", async (req, res, next) => {
+notesRouter.get("/:id", async (req, res) => {
 	const note = await Note.findById(req.params.id);
 	if (note) {
 		res.json(note);
@@ -16,13 +17,27 @@ notesRouter.get("/:id", async (req, res, next) => {
 	}
 });
 
-notesRouter.post("/", async (req, res, next) => {
+const getTokenFrom = req => {
+	const authorization = req.get("authorization");
+	if (authorization && authorization.toLowerCase().startsWith("bearer")) {
+		console.log(authorization);
+		return authorization.substring(7);
+	}
+	return null;
+};
+
+notesRouter.post("/", async (req, res) => {
 	const body = req.body;
-	const user = await User.findById(body.userId);
+	const token = getTokenFrom(req);
+	const decodedToken = jwt.verify(token, process.env.SECRET);
+	if (!decodedToken.id) {
+		return res.status(401).json({ error: "token missing or invalid " });
+	}
+	const user = await User.findById(decodedToken.id);
 
 	const note = new Note({
 		content: body.content,
-		important: body.important || false,
+		important: body.important === undefined ? false : body.important,
 		date: new Date(),
 		user: user._id,
 	});
@@ -31,10 +46,10 @@ notesRouter.post("/", async (req, res, next) => {
 	user.notes = user.notes.concat(savedNote._id);
 	await user.save();
 
-	res.status(201).json(savedNote);
+	res.json(savedNote);
 });
 
-notesRouter.delete("/:id", async (req, res, next) => {
+notesRouter.delete("/:id", async (req, res) => {
 	await Note.findByIdAndRemove(req.params.id);
 	res.status(204).end();
 });
